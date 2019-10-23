43ee42cc891da37a0d152ad6bd3e7793d0470913
Deniz <mersinliogludeniz@gmail.com>
2019-10-23 11:35:42 +0300

    Initial Commit

---
+++ MoviesSwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoviesSwiftUIDemo
+//
+//  Created by Deniz Mersinlioğlu on 23.10.2019.
+//  Copyright © 2019 DevelopmentHouse. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
