8db467a188097f81445c4b2ec4ba5c12f5c5e6a9
Deniz <mersinliogludeniz@gmail.com>
2019-10-23 14:00:10 +0300

    Movie List implemented

--- MoviesSwiftUIDemo/ContentView.swift
+++ MoviesSwiftUIDemo/ContentView.swift
@@ -8,9 +8,40 @@
 
 import SwiftUI
 
+struct Movie: Identifiable, Hashable {
+    let id = UUID()
+    let name, imageName: String
+}
+
 struct ContentView: View {
+    
+    let movies: [[Movie]] = [
+        [Movie(name: "Deadpool 2", imageName: "movie0"),
+         Movie(name: "The Shape of Water", imageName: "movie1")],
+        [Movie(name: "Incredibles 2", imageName: "movie2"),
+         Movie(name: "Tomb Raider", imageName: "movie3")],
+        [Movie(name: "Deadpool 2", imageName: "movie0"),
+         Movie(name: "The Shape of Water", imageName: "movie1")],
+        [Movie(name: "Incredibles 2", imageName: "movie2"),
+         Movie(name: "Tomb Raider", imageName: "movie3")]]
+    
     var body: some View {
-        Text("Hello World")
+        GeometryReader{ geo in
+            ScrollView{
+                VStack(spacing: 48){
+                    Text("Dev House Movie")
+                        .font(.system(size: 20, weight: .semibold))
+                    ForEach(self.movies, id: \.self){ row in
+                        HStack(spacing: 16) {
+                            ForEach (row) { movie in
+                                MovieView(movie: movie, size: geo.size)
+                            }
+                        }
+                    }
+                    Spacer()
+                }.padding(.horizontal, 16)
+            }
+        }
     }
 }
 
@@ -19,3 +50,37 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct MovieView: View{
+    let movie: Movie
+    let size: CGSize
+    
+    var body: some View{
+        
+        VStack (spacing: 8){
+            Image(movie.imageName)
+                .resizable()
+                .scaledToFill()
+                .frame(width: (size.width - 48)/2, height: 250)
+                .clipped()
+                .cornerRadius(8)
+                .shadow(radius: 10)
+            Text(movie.name)
+                .font(.system(size: 15, weight: .bold))
+            Button(action: {
+                print("Button Clicked")
+            }) {
+                Text("Buy Tickets")
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 16)
+                    .padding(.vertical, 4)
+                    .background(Color.red)
+                    .cornerRadius(8)
+                    .shadow(radius: 10)
+            }
+        }
+    }
+}
+
+
+
